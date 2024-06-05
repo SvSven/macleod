67a89dc3791b7f843d8918578e22c1a956e3cd6f
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-06-05 13:25:43 +0200

    🚧 initial header structure for homepage

--- studio/schemaTypes/singletons/home.tsx
+++ studio/schemaTypes/singletons/home.tsx
@@ -4,12 +4,56 @@ export const Home = defineType({
   title: 'Home',
   name: 'home',
   type: 'document',
+  fieldsets: [
+    {
+      name: 'header',
+      title: 'Header',
+    },
+  ],
   fields: [
     defineField({
       type: 'string',
       name: 'title',
       title: 'Title',
       validation: (rule) => rule.required(),
+      fieldset: 'header',
+    }),
+    defineField({
+      type: 'text',
+      name: 'ingress',
+      title: 'Text',
+      validation: (rule) => rule.required(),
+      fieldset: 'header',
+    }),
+    defineField({
+      type: 'imageWithAlt',
+      name: 'headerImage',
+      title: 'Header image',
+      validation: (rule) => rule.required(),
+      fieldset: 'header',
+    }),
+    defineField({
+      type: 'object',
+      name: 'cta',
+      title: 'Call to action',
+      fields: [
+        defineField({
+          type: 'string',
+          name: 'label',
+          title: 'Button label',
+          validation: (rule) => rule.required(),
+        }),
+        defineField({
+          type: 'reference',
+          name: 'link',
+          title: 'Link',
+          to: [{ type: 'page' }],
+          validation: (rule) => rule.required(),
+          options: { disableNew: true },
+        }),
+      ],
+      options: { columns: 2 },
+      fieldset: 'header',
     }),
   ],
 })
