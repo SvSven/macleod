c2840363eb98c97ef6a7fb82200537d7751926c6
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-06-10 13:20:14 +0200

    ✨ add utility list with colors to studio

---
+++ studio/src/colors.ts
@@ -0,0 +1,53 @@
+export const sectionBackgrounds = [
+  {
+    title: 'White',
+    value: 'hsl(0, 0%, 100%)',
+    key: 'white-100',
+  },
+  {
+    title: 'Light blue',
+    value: 'hsl(196, 38%, 80%)',
+    key: 'blue-100',
+  },
+  {
+    title: 'Light green',
+    value: 'hsl(182, 38%, 80%)',
+    key: 'green-100',
+  },
+  {
+    title: 'Light yellow',
+    value: 'hsl(44, 78%, 95%)',
+    key: 'yellow-100',
+  },
+]
+
+export const componentBackgrounds = [
+  {
+    title: 'White',
+    value: 'hsl(0, 0%, 100%)',
+    key: 'white-100',
+  },
+  {
+    title: 'Blue',
+    value: 'hsl(196, 38%, 40%)',
+    key: 'blue-900',
+  },
+  {
+    title: 'Green',
+    value: 'hsl(182, 38%, 40%)',
+    key: 'green-900',
+  },
+]
+
+export const buttonColors = [
+  {
+    title: 'Red',
+    value: 'hsl(7, 61%, 50%)',
+    key: 'red',
+  },
+  {
+    title: 'Yellow',
+    value: 'hsl(44, 78%, 95%)',
+    key: 'yellow-900',
+  },
+]
