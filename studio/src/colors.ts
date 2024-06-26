5f9bf25636f67ea2ae5a5edf34775146148d967d
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-06-26 14:21:26 +0200

    🔧 update background color keys in line with tailwind

--- studio/src/colors.ts
+++ studio/src/colors.ts
@@ -2,22 +2,22 @@ export const sectionBackgrounds = [
   {
     title: 'White',
     value: 'hsl(0, 0%, 100%)',
-    key: 'white-100',
+    key: 'white-50',
   },
   {
     title: 'Light blue',
     value: 'hsl(196, 38%, 80%)',
-    key: 'blue-100',
+    key: 'blue-50',
   },
   {
     title: 'Light green',
     value: 'hsl(182, 38%, 80%)',
-    key: 'green-100',
+    key: 'green-50',
   },
   {
     title: 'Light yellow',
     value: 'hsl(44, 78%, 95%)',
-    key: 'yellow-100',
+    key: 'yellow-50',
   },
 ]
 
@@ -25,7 +25,7 @@ export const componentBackgrounds = [
   {
     title: 'White',
     value: 'hsl(0, 0%, 100%)',
-    key: 'white-100',
+    key: 'white-50',
   },
   {
     title: 'Blue',
@@ -43,7 +43,7 @@ export const buttonColors = [
   {
     title: 'Red',
     value: 'hsl(7, 61%, 50%)',
-    key: 'red',
+    key: 'red-900',
   },
   {
     title: 'Yellow',
