da10d883eca6b9076a4a5a8fcc466627872f46fa
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-06-28 13:22:16 +0200

    🚧 add CTA to home header

--- web/src/components/HeaderHome.tsx
+++ web/src/components/HeaderHome.tsx
@@ -1,6 +1,7 @@
 import { SanityImage } from '@/components/SanityImage'
 import HotspotToObjPos from '@/utils/HotspotToObjPos'
 import type { CTA, ImageWithAlt } from '@/types'
+import ButtonLink from './ButtonLink'
 
 type Props = {
   ingress: string
@@ -9,12 +10,18 @@ type Props = {
   cta: CTA
 }
 
-export const HeaderHome = ({ ingress, title, image }: Props) => {
+export const HeaderHome = ({ ingress, title, image, cta }: Props) => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 text-white bg-mcl-blue-900 min-h-[50vh]">
       <div className="max-w-[720px] py-16 px-8 justify-self-end flex flex-col justify-center order-2 md:order-1">
-        <h1 className="text-4xl font-bold mb-6">{title}</h1>
-        <p>{ingress}</p>
+        <h1 className="text-4xl font-bold mb-8">{title}</h1>
+        <p className="mb-8">{ingress}</p>
+
+        {cta && (
+          <div>
+            <ButtonLink url="">{cta.label}</ButtonLink>
+          </div>
+        )}
       </div>
       <div className="relative min-h-[30vh] order-1 md:order-2">
         <SanityImage
