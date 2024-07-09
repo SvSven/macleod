49e244bed22481016e1c5235fb22c28d190aca55
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-07-09 11:13:31 +0200

    💄 typography tweaks in header

--- web/src/components/HeaderHome.tsx
+++ web/src/components/HeaderHome.tsx
@@ -13,9 +13,9 @@ type Props = {
 export const HeaderHome = ({ ingress, title, image, cta }: Props) => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 text-white bg-mcl-blue-900 min-h-[50vh]">
-      <div className="max-w-[720px] py-16 px-8 justify-self-end flex flex-col justify-center order-2 md:order-1">
-        <h1 className="text-4xl font-bold mb-8">{title}</h1>
-        <p className="mb-8">{ingress}</p>
+      <div className="max-w-[720px] py-12 md:py-16 px-8 justify-self-end flex flex-col justify-center order-2 md:order-1">
+        <h1 className="text-4xl md:text-5xl font-bold mb-8 leading-tight">{title}</h1>
+        <p className="mb-8 tracking-wide leading-loose">{ingress}</p>
 
         {cta && (
           <div>
