b7490450c80800668ba0952901095fa9ea3cbc66
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-07-12 22:52:16 +0200

    🚧 begin work on highlighted links in home header

--- web/src/components/HeaderHome.tsx
+++ web/src/components/HeaderHome.tsx
@@ -2,6 +2,7 @@ import { SanityImage } from '@/components/SanityImage'
 import HotspotToObjPos from '@/utils/HotspotToObjPos'
 import type { CTA, ImageWithAlt } from '@/types'
 import ButtonLink from './ButtonLink'
+import { Arrow } from './Arrow'
 
 type Props = {
   ingress: string
@@ -10,20 +11,40 @@ type Props = {
   cta: CTA
 }
 
+const actions = [
+  {
+    title: 'Het Schotse uurtje',
+    text: 'Het Schotse uurtje is een uur lang entertainment met Schotse muziek',
+    link: 'fubar',
+  },
+  {
+    title: 'De MacLeod Boys',
+    text: 'Wie zijn de MacLeod Boys? Waar komen ze vandaan? En hebben ze weleens Haggis gegeten?',
+    link: 'fubar',
+  },
+  {
+    title: "Foto's & video's",
+    text: 'Benieuwd naar een impressie van de MacLeod Boys? Bekijk dan onze foto’s en video’s',
+    link: 'fubar',
+  },
+]
+
 export const HeaderHome = ({ ingress, title, image, cta }: Props) => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 text-white bg-mcl-blue-900 min-h-[50vh]">
-      <div className="max-w-[720px] py-12 md:py-16 px-8 justify-self-end flex flex-col justify-center order-2 md:order-1">
-        <h1 className="text-4xl md:text-5xl font-bold mb-8 leading-tight">{title}</h1>
-        <p className="mb-8 tracking-wide leading-loose">{ingress}</p>
+    <section className="relative grid min-h-[50lvh] grid-cols-1 text-white md:grid-cols-2">
+      <div className="bg-mcl-blue-900 order-2 grid w-full md:order-1 md:pb-20">
+        <div className="flex max-w-[640px] flex-col justify-center justify-self-end px-8 py-12 md:py-16">
+          <h1 className="mb-8 text-4xl font-bold leading-tight md:text-5xl">{title}</h1>
+          <p className="mb-8 leading-loose tracking-wide">{ingress}</p>
 
-        {cta && (
-          <div>
-            <ButtonLink url="">{cta.label}</ButtonLink>
-          </div>
-        )}
+          {cta && (
+            <div>
+              <ButtonLink url="">{cta.label}</ButtonLink>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="relative min-h-[30vh] order-1 md:order-2">
+      <div className="relative order-1 min-h-[30vh] md:order-2 md:pb-20">
         <SanityImage
           src={image}
           alt={image.alt || ''}
@@ -31,6 +52,22 @@ export const HeaderHome = ({ ingress, title, image, cta }: Props) => {
           className="object-cover"
           style={{ objectPosition: HotspotToObjPos(image.hotspot) }}
         />
+      </div>
+      <div className="z-10 order-3 md:col-span-2 md:-mt-20">
+        <div className="text-mcl-blue-900 flex max-w-screen-xl flex-col divide-y-2 divide-slate-100 rounded-lg border-gray-200 bg-white shadow-lg md:mx-auto md:flex-row md:divide-x-2 md:divide-y-0">
+          {actions.map(({ title, text, link }) => (
+            <a
+              href={link}
+              className="duration-50 flex grow basis-0 flex-col p-6 text-center transition first:rounded-l-lg last:rounded-r-lg hover:bg-slate-50"
+            >
+              <h2 className="text-1xl mb-3 font-bold md:text-2xl">{title}</h2>
+              <p className="mb-3">{text}</p>
+              <div className="ml-auto mt-auto">
+                <Arrow />
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
     </section>
   )
--- web/src/templates/homepage.tsx
+++ web/src/templates/homepage.tsx
@@ -2,7 +2,12 @@ import type { HomepageProps } from '@/types'
 import { HeaderHome } from '@/components/HeaderHome'
 
 const HomePage = ({ header }: HomepageProps) => {
-  return <HeaderHome {...header} />
+  return (
+    <>
+      <HeaderHome {...header} />
+      <h1 className="pt-10">Hello world</h1>
+    </>
+  )
 }
 
 export default HomePage
